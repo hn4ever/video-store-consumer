c982b553f45989e179d98bc74a44cea114738fdf
Noor B.A <noor.bahr.aluloom@gmail.com>
2021-01-21 18:29:53 -0800

    created on mouse over functionalites to desplay details of customers and movies

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Video.css'
 
 
 const Video = (props) => {
-
+    const [showVideoDetails, setShowVideoDetails] = useState(false)
     const onSelect = () => {
         props.onSelectVideoCallback(props.id);
     }
@@ -11,11 +11,16 @@ const Video = (props) => {
     return (
         <div className = "video">
             <h2>{props.title}</h2>
+            <img className = 'image'src={props.image_url} alt='video cover'
+                onMouseEnter={() => setShowVideoDetails(true)}
+                onMouseLeave={() => setShowVideoDetails(false)}/>
+            {showVideoDetails && (
             <ul>
-                <img className = 'image'src={props.image_url} alt='video cover'/>
-                {/* <p className= 'overview'>Overview: {props.overview}</p>
-                <p>Release Date: {props.release_date}</p> */}
+                <p className= 'overview'>Overview: {props.overview}</p>
+                <p>Release Date: {props.release_date}</p>
             </ul>
+            )}
+
             <button onClick={onSelect}> Select </button>
         </div>
     )
